fef7c39c03fa242104882885f772b0cf07baa318
Fernando Baptista <fernandohsb@gmail.com>
2015-05-06 15:40:01 -0300

    sprite kit tutorial 2

---
+++ SpriteKit2/Breakout/Breakout/GameOverScene.swift
@@ -0,0 +1,30 @@
+//
+//  GameOverScene.swift
+//  Breakout
+//
+//  Created by Fernando on 5/5/15.
+//  Copyright (c) 2015 Fernando. All rights reserved.
+
+
+
+import SpriteKit
+
+let GameOverLabelCategoryName = "gameOverLabel"
+
+class GameOverScene: SKScene {
+    var gameWon : Bool = false {
+        // 1.
+        didSet {
+            let gameOverLabel = childNodeWithName(GameOverLabelCategoryName) as! SKLabelNode
+            gameOverLabel.text = gameWon ? "Game Won" : "Game Over"
+        }
+    }
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        if let view = view {
+            // 2.
+            let gameScene = GameScene.unarchiveFromFile("GameScene") as! GameScene
+            view.presentScene(gameScene)
+        }
+    }
+}
